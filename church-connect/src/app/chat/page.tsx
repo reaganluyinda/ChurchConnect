76d7acd55a7fc934685a5f5bacc1a42f3314a0f3
reagan luyinda <luyindareagan@gmail.com>
2026-01-05 13:50:35 +0300

    Feat; Create chat component
    create chat component for the chat route

---
+++ church-connect/src/app/chat/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import UserButton from "../components/userButton";
+
+function Chat() {
+  return (
+    <div>
+      <h1 className="text-3xl items-center">Chat page</h1>
+    </div>
+  );
+}
+
+export default Chat;
